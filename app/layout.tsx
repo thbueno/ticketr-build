92a2e0f6caf38149b52d64d73902e61c45d4325f
thbueno <thiagobueno.s@gmail.com>
2025-01-16 13:12:02 +0700

    Add initial implementation of Convex and Clerk integration with utility functions and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>
+          <ClerkProvider>{children}</ClerkProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://workable-oyster-95.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};

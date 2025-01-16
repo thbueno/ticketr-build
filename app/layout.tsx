b919e223ec2cab750169c1e3b05915b44a9c3b06
thbueno <thiagobueno.s@gmail.com>
2025-01-16 19:57:13 +0700

    Add SyncUserWithConvex component and user schema; update Convex API integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import Header from "@/components/Header";
+import SyncUserWithConvex from "@/components/SyncUserWithConvex";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -33,6 +34,7 @@ export default function RootLayout({
         <ConvexClientProvider>
           <ClerkProvider>
             <Header />
+            <SyncUserWithConvex />
             {children}
           </ClerkProvider>
         </ConvexClientProvider>

17c35b0b26e9bbaf2d2a513bf407b42f388c8ba1
thbueno <thiagobueno.s@gmail.com>
2025-03-07 23:41:25 +0700

    Add user context and routing parameters to EventPage component

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,4 +1,10 @@
 "use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
+
 export function EventPage() {
+  const { user } = useUser();
+  const params = useParams();
   return <div>page</div>;
 }

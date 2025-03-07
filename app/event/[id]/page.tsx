fabbf9faae7ba936eacead8b5ad896e1bec876b1
thbueno <thiagobueno.s@gmail.com>
2025-03-07 23:42:47 +0700

    Fetch event details and availability in EventPage component

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
 import { useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
 
 export function EventPage() {
   const { user } = useUser();
   const params = useParams();
+
+  const event = useQuery(api.events.getById, {
+    eventId: params.id as Id<"events">,
+  });
+  const availability = useQuery(api.events.getEventAvailability, {
+    eventId: params.id as Id<"events">,
+  });
   return <div>page</div>;
 }

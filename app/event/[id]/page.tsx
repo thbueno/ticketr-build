2b65bb219f50ba7695cdeef6439475e0f8e0d3f9
thbueno <thiagobueno.s@gmail.com>
2025-03-08 00:16:30 +0700

    Enhance EventPage component with loading spinner and event image display

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import Spinner from "@/components/Spinner";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useStorageUrl } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 
-export function EventPage() {
+export default function EventPage() {
   const { user } = useUser();
   const params = useParams();
 
@@ -16,5 +19,35 @@ export function EventPage() {
   const availability = useQuery(api.events.getEventAvailability, {
     eventId: params.id as Id<"events">,
   });
-  return <div>page</div>;
+
+  const imageUrl = useStorageUrl(event?.imageStorageId);
+
+  if (!event || !availability) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+  return (
+    <div className="min-h-screen bg-gray-50">
+      {/* Events details */}
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="bg-white rounded-xl shadow-sm overflow-hidden">
+          {/* Event Image */}
+          {imageUrl && (
+            <div className="aspect-[21/9] relative w-full">
+              <Image
+                src={imageUrl}
+                alt={event.name}
+                fill
+                className="object-cover"
+                priority
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }

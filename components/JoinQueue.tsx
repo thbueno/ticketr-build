05f036f7f60ead17a7d501aa6c51b4e6cbd4c198
thbueno <thiagobueno.s@gmail.com>
2025-03-13 11:32:09 +0700

    Add joinWaitingList mutation and checkAvailability query for event ticket management

--- components/JoinQueue.tsx
+++ components/JoinQueue.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
 // import { toast } from "sonner"
 
 function JoinQueue({
@@ -9,6 +11,15 @@ function JoinQueue({
   eventId: Id<"events">;
   userId: string;
 }) {
+  const joinWaitingList = useMutation(api.events.joinWaitingList);
+  const queuePosition = useQuery(api.waitingList.getQueuePosition, {
+    eventId,
+    userId,
+  });
+  const userTicket = useQuery(api.tickets.getUserTicketForEvent, {
+    eventId,
+    userId,
+  });
   return <div>JoinQueue</div>;
 }
 
